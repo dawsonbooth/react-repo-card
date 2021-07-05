9cb890b98890bc2451ee1908638693f5fa797723
dawsonbooth <github@dawsonbooth.com>
2021-07-05 14:34:36 -0500

    Fix crash on API error

--- src/index.tsx
+++ src/index.tsx
@@ -24,6 +24,7 @@ export const useFetch = (url: string): [any, boolean, boolean] => {
         setLoading(false);
       });
   }, [url]);
+  console.log(response);
   return [response, loading, hasError];
 };
 
@@ -120,6 +121,46 @@ const RepoCard: React.FC<RepoCardPropTypes> = ({
         />
       );
     });
+  else {
+    const name = `${username}/${repository}`;
+    const url = `https://github.com/${name}`;
+    return (
+      <div
+        style={{
+          fontFamily:
+            "-apple-system,BlinkMacSystemFont,Segoe UI,Helvetica,Arial,sans-serif,Apple Color Emoji,Segoe UI Emoji",
+          border: "1px solid #e1e4e8",
+          borderRadius: "6px",
+          background: "white",
+          padding: "16px",
+          fontSize: "14px",
+          lineHeight: "1.5",
+          color: "#24292e"
+        }}
+      >
+        <div style={{ display: "flex", alignItems: "center" }}>
+          <svg
+            style={{ fill: "#586069", marginRight: "8px" }}
+            viewBox="0 0 16 16"
+            version="1.1"
+            width={16}
+            height={16}
+            aria-hidden="true"
+          >
+            <path
+              fillRule="evenodd"
+              d="M2 2.5A2.5 2.5 0 014.5 0h8.75a.75.75 0 01.75.75v12.5a.75.75 0 01-.75.75h-2.5a.75.75 0 110-1.5h1.75v-2h-8a1 1 0 00-.714 1.7.75.75 0 01-1.072 1.05A2.495 2.495 0 012 11.5v-9zm10.5-1V9h-8c-.356 0-.694.074-1 .208V2.5a1 1 0 011-1h8zM5 12.25v3.25a.25.25 0 00.4.2l1.45-1.087a.25.25 0 01.3 0L8.6 15.7a.25.25 0 00.4-.2v-3.25a.25.25 0 00-.25-.25h-3.5a.25.25 0 00-.25.25z"
+            />
+          </svg>
+          <span style={{ fontWeight: 600, color: "#0366d6" }}>
+            <a style={{ textDecoration: "none", color: "inherit" }} href={url}>
+              {name}
+            </a>
+          </span>
+        </div>
+      </div>
+    );
+  }
 
   return (
     <div
